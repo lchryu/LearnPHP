8d7941f1448c65816808882487de74396b3e71cc
Lucute2001 <lch.ryu2001@gmail.com>
2023-05-21 20:19:20 +0700

    update

--- 4-DiemDanh/app/controllers/AttendanceController.php
+++ 4-DiemDanh/app/controllers/AttendanceController.php
@@ -4,6 +4,6 @@ class AttendanceController {
     public function markAttendance() {
         // Process the attendance marking logic
         // Retrieve attendance data from the request, update the database, etc.
-        // Redirect or render appropriate view
+        // Redirect or ren  der appropriate view
     }
 }
